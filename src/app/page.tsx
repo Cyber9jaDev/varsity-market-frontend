afdc98494d88f2cbd1aab2d6bad7efbfeb7eedfb
cyber9ja <ayodejioladapo15@gmail.com>
2024-07-26 17:19:52 +0100

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-import Categories from "@/components/Categories";
-import FooterTop from "@/components/FooterTop";
-import HeroBanner from "@/components/HeroBanner";
-import WhyWeAreUnique from "@/components/WhyWeAreUnique";
 import styles from './styles/styles.module.scss';
+import Image from "next/image";
+import img from '../assets/campus4.jpg' 
+import HeroBanner from '@/components/HeroBanner';
+import Categories from '@/components/Categories';
+import WhyWeAreUnique from '@/components/WhyWeAreUnique';
+import FooterTop from '@/components/FooterTop';
+
 
 export default function Home() {
   return (
     <main className={styles.home}>
+      <div className={styles["bg-image"]}>
+        <Image src={img} alt="bg-image"/>
+      </div>
       <HeroBanner />
       <Categories />
       <WhyWeAreUnique />
