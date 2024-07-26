3b8976b55038b4ed2cd6f3ff71cc6e70e1f12a9e
cyber9ja <ayodejioladapo15@gmail.com>
2024-07-26 17:57:16 +0100

    Updated why we are unique

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-import styles from './styles/styles.module.scss';
+import styles from "./styles/styles.module.scss";
 import Image from "next/image";
-import img from '../assets/campus4.jpg' 
-import HeroBanner from '@/components/HeroBanner';
-import Categories from '@/components/Categories';
-import WhyWeAreUnique from '@/components/WhyWeAreUnique';
-import FooterTop from '@/components/FooterTop';
-
+import img from "../assets/campus4.jpg";
+import HeroBanner from "@/components/HeroBanner";
+import Categories from "@/components/Categories";
+import WhyWeAreUnique from "@/components/WhyWeAreUnique";
+import FooterTop from "@/components/FooterTop";
 
 export default function Home() {
   return (
     <main className={styles.home}>
       <div className={styles["bg-image"]}>
-        <Image src={img} alt="bg-image"/>
+        <Image src={img} alt="bg-image" />
       </div>
       <HeroBanner />
       <Categories />
--- src/components/WhyWeAreUnique.tsx
+++ src/components/WhyWeAreUnique.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase, faHandshake, faBoxesStacked } from '@fortawesome/free-solid-svg-icons';
 import './styles/WhyWeAreUnique.scss';
-
 
 const WhyWeAreUnique = () => {
   return (
@@ -9,25 +10,23 @@ const WhyWeAreUnique = () => {
       <p>We are dedicated to give that effortless platform where only student RULES. <br /> Student, student and ONLY student.</p>
       <div className='mt-5 container unique-details'>
         <div className="details-wrapper">
-          <i className="fa fa-solid fa-briefcase icon-1"></i>
+          <FontAwesomeIcon icon={faBriefcase} className="fa icon-1" />
           <h4 className='my-3'>Make deals within your campus</h4>
           <span className='text-center'>You don't need to go far from your college campus.</span>
         </div>
         <div className="details-wrapper">
-          <i className="fa fa-solid fa-handshake icon-2"></i>
+          <FontAwesomeIcon icon={faHandshake} className="fa icon-2" />
           <h4 className='my-3'>Trusted Purchase</h4>
           <span className='text-center'>Awesome! You are dealing with your college mate or any other near you.</span>
         </div>
         <div className="details-wrapper">
-          <i className="fa fa-solid fa-boxes-stacked icon-3"></i>
+          <FontAwesomeIcon icon={faBoxesStacked} className="fa icon-3" />
           <h4 className='my-3'>Multiple Products in a Single Ad.</h4>
           <span className='text-center'>You have books, notes, stationery, bikes to sell. Don't worry you can add these all items in a Single Ad.</span>
         </div>
-        
       </div>
-      
     </section>
   )
 }
 
-export default WhyWeAreUnique
+export default WhyWeAreUnique;
