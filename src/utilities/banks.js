499f09512e539aca94b58082a5dcf31a06e59b75
cyber9ja <ayodejioladapo15@gmail.com>
2024-11-18 13:20:22 +0000

    minor fix

---
+++ src/utilities/banks.js
@@ -0,0 +1,42 @@
+const BankList = [
+  {
+    "name": "Access Bank",
+    "slug": "access-bank",
+    "code": "044"
+  },
+  {
+    "name": "First Bank of Nigeria",
+    "slug": "first-bank-of-nigeria",
+    "code": "011"
+  },
+  {
+    "name": "Guaranty Trust Bank",
+    "slug": "guaranty-trust-bank",
+    "code": "058"
+  },
+  {
+    "name": "Kuda Bank",
+    "slug": "kuda-bank",
+    "code": "50211"
+  },
+  {
+    "name": "OPay Digital Services Limited (OPay)",
+    "slug": "paycom",
+    "code": "999992"
+  },
+  {
+    "name": "PalmPay",
+    "slug": "palmpay",
+    "code": "999991"
+  },
+  {
+    "name": "Union Bank of Nigeria",
+    "slug": "union-bank-of-nigeria",
+    "code": "032"
+  },
+  {
+    "name": "Zenith Bank",
+    "slug": "zenith-bank",
+    "code": "057"
+  }
+]
