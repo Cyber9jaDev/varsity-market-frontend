f2afb85867efa48ef1e5f22a5ae01dfc675020b7
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-26 11:42:19 +0000

    update product lists form

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -113,11 +113,11 @@ const Product = () => {
     <section className="container-fluid" id="products">
       <div className="container">
         {filterModalIsOpen === false &&
-          <div className="border d-block p-2">
+          <header className="border d-block p-2">
             <p className="category-label d-inline-block m-0 me-2 fs-5"> Categories </p>
             <p className="d-inline-block m-0 me-2 fs-5"> / </p>
             <p className="category-label d-inline-block m-0 fs-5">{findCategoryLabel(category, values)} </p>
-          </div>
+          </header>
         }
       </div>
 
@@ -137,7 +137,7 @@ const Product = () => {
             <form className="form" onSubmit={getProducts}>
               <div className="container">
                 <div className="row">
-                  <div className="col-12 my-3">
+                  <div className="col-12 my-2">
                     <div className="form-group">
                       <label htmlFor="searchText"> Search for a specific product</label>
                       <div className="search-container">
@@ -145,41 +145,41 @@ const Product = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="col-12 my-3">
+                  <div className="col-12 my-2">
                     <div className="form-group">
                       <label htmlFor="dateFrom"> Date from: </label>
                       <input defaultValue={values.dateFrom} className="w-100 date-picker" type='date' onChange={(e) => updateFilter(e.target.value, 'dateFrom')} />
                     </div>
                   </div>
-                  <div className="col-12 my-3">
+                  <div className="col-12 my-2">
                     <div className="form-group">
                       <label htmlFor="dateTo"> Date to: </label>
                       <input defaultValue={values.dateTo} className="w-100 date-picker" type='date' onChange={(e) => updateFilter(e.target.value, 'dateTo')} />
                     </div>
                   </div>
-                  <div className="col-12 my-3">
+                  <div className="col-12 my-2">
                     <div className="form-group">
                       <label htmlFor="location"> Select your school to see local ads</label>
-                      <select onChange={(e) => { updateFilter(e.target.value, 'location') }} name="location" id="location" defaultValue={location} className="w-100 py-2 border-0">
+                      <select onChange={(e) => { updateFilter(e.target.value, 'location') }} name="location" id="location" defaultValue={location} className="w-100 py-2">
                         {schools.map(({ value, label }) => <option key={value} value={value}>{label}</option>)}
                       </select>
                     </div>
                   </div>
-                  <div className="col-12 my-3">
+                  <div className="col-12 my-2">
                     <div className="form-group select">
                       <label htmlFor="category">Browse Categories</label>
-                      <select onChange={(e) => { updateFilter(e.target.value, 'category') }} name="category" id="category" defaultValue={category} className="w-100 py-2 border-0">
+                      <select onChange={(e) => { updateFilter(e.target.value, 'category') }} name="category" id="category" defaultValue={category} className="w-100 py-2">
                         {categories.map(({ value, label }) => <option key={value} value={value}>{label}</option>)}
                       </select>
                     </div>
                   </div>
-                  <div className="col-12 my-3">
+                  <div className="col-12 my-2">
                     <label className="d-block"> Order By <span> :</span>{" "} </label>
-                    <select onChange={(e) => { updateFilter(e.target.value, 'orderBy') }} name="orderBy" id="orderBy" defaultValue={values.orderBy} className="w-100 py-2 border-0">
+                    <select onChange={(e) => { updateFilter(e.target.value, 'orderBy') }} name="orderBy" id="orderBy" defaultValue={values.orderBy} className="w-100 py-2">
                       {orderBy.map(({ value, label }) => <option key={value} value={value}>{label}</option>)}
                     </select>
                   </div>
-                  <div className="col-12 my-3">
+                  <div className="col-12 my-2">
                     <div className="form-group">
                       <label htmlFor="price" className="w-100 d-flex">
                         <span style={{ width: '40%', fontWeight: '600' }}>{formatNaira(values.price.min)}</span>{" "}
@@ -191,7 +191,7 @@ const Product = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="col-12 my-3">
+                  <div className="col-12 my-2">
                     <button className="search-btn" type="submit"> Apply Filter </button>
                   </div>
                 </div>
